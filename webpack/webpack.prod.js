d5ab53c613e0e3b34eaae51b0e2e9b7de8ee5c9f
Abdiel A Flores <abdielantonio.af@gmail.com>
2023-05-14 14:44:06 -0500

    add enviroment aplication  env

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -29,6 +29,11 @@ const envKeys = (enviromentExist === true ? (
 module.exports = {
   mode: 'production',
   devtool: 'source-map',
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
   plugins: [
     new webpack.DefinePlugin(envKeys),
   ],
